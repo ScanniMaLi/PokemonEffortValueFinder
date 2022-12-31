61a6749c28952a87b7e72372381015c14199fba3
ScanniMaLi <Scanni@MaLi>
2022-12-31 14:49:36 +0100

    added  launcher.

---
+++ PokemonEffortValueFinder.Launcher/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+const string exePath = "./_Data/PokemonEffortValueFinder.exe";
+Process process = new Process();
+process.StartInfo.FileName = exePath;
+process.StartInfo.UseShellExecute = false;
+process.StartInfo.Arguments = "run";
+process.Start();
